febc0af3764b697f326c75e2d092e4a6d34988ce
Apurbo <apurbo4t4@gmail.com>
2025-10-03 01:40:47 +0600

    deploy error resolved

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 AUTH_USER_MODEL='users.User'
 
